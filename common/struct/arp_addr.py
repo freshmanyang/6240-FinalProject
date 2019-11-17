f4dc1d99fe367a5c5b98921fd6c366f855bfb677
yanghanjie <yang_hanjie@163.com>
2019-11-17 14:54:03 -0500

    rm arp_addr.py

--- common/struct/arp_addr.py
+++
@@ -1,6 +0,0 @@
-class arp_addr:
-    def __init__(src_mac="",src_ip="",dst_mac="",dst_ip=""):
-        self.src_mac=src_mac
-        self.src_ip=src_ip
-        self.dst_mac=dst_mac
-        self.dst_ip=dst_ip
---
+++ common/struct/struct.py
@@ -0,0 +1,6 @@
+class Arp:
+    def __init__(self, src_mac_=None, src_ip_=None, dest_mac_=None, dest_ip_=None):
+        self.src_mac = src_mac_
+        self.src_ip = src_ip_
+        self.dest_mac = dest_mac_
+        self.dest_ip = dest_ip_
